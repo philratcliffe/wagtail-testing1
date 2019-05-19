613d3d1492d85145e4861c22170a93e90747c2f8
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-05-19 16:05:18 +0100

    Sort blog posts as expected

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-from wagtail.core.models import Page
+
+# New imports added for ParentalKey, Orderable, InlinePanel, ImageChooserPanel
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 
 
@@ -11,6 +16,13 @@ class BlogIndexPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
     ]
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
 
 class BlogPage(Page):
     date = models.DateField("Post date")
