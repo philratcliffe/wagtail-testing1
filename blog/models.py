41ce3e9cb5ca09203422da1bb82f5d194db76170
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-05-22 11:39:51 +0100

    More work around images

--- blog/models.py
+++ blog/models.py
@@ -30,6 +30,13 @@ class BlogPage(Page):
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
 
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
         index.SearchField('body'),
@@ -41,6 +48,7 @@ class BlogPage(Page):
         FieldPanel('body', classname="full"),
         InlinePanel('gallery_images', label="Gallery images"),
     ]
+
 
 class BlogPageGalleryImage(Orderable):
     page = ParentalKey(BlogPage, on_delete=models.CASCADE,
